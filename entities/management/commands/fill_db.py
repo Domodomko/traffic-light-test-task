62dbe6a5110773b5f2118d374ba78fc3cec4eb4d
Sergei Shinn <shinn.sergey@dteam.dev>
2021-11-29 00:54:18 +0600

    Fixed swagger urls; Set correct numbers for fill_db command;

--- entities/management/commands/fill_db.py
+++ entities/management/commands/fill_db.py
@@ -10,8 +10,8 @@ from entities.factories import ClientFactory, EntityFactory, DepartmentFactory
 
 FAKER = faker.Faker()
 
-CLIENTS_NUM = 30
-ENTITIES_NUM = 30
+CLIENTS_NUM = 30000
+ENTITIES_NUM = 30000
 DEPARTMENTS_NUM = 100  # It creates 7 items at a time
 CLIENTS_PER_DEPARTMENT = 5
 
